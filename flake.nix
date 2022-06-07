fb17ba4d867aed67da92840dc08735db0e0a9a2c
darkn07 <darkn4754+mail@gmail.com>
2022-06-07 13:55:51 +0300

    Fix to make VS Code Server work on NixOS

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    nixos-vscode-server.url = "github:darkn07/nixos-vscode-server";
   };
 
   outputs = inputs@{ self, utils, nixpkgs, unstable, home-manager, ... }:
@@ -29,6 +30,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
           }
+          nixos-vscode-server.nixosModules.system
         ];
       };
 
